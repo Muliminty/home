1a341cff9a26c1b50d46674c295624c86afaa67b
muliminty <313195303@qq.com>
2024-08-14 21:53:34 +0800

    首页样式

--- src/home/App.jsx
+++ src/home/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-    <>
-      Muliminty
-    </>
-  )
+    <div className="container">
+      <div>
+        <h1 className="title">Welcome to Muliminty</h1>
+        <p className="subtext">希望我们都能保持清醒和有意义的生活😁</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
