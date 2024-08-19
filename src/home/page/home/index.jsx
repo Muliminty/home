4f7e240556a4841ae6a9eb2874bdc8d80a30279d
muliminty <313195303@qq.com>
2024-08-19 18:09:23 +0800

    测试路由切换

--- src/home/page/home/index.jsx
+++ src/home/page/home/index.jsx
@@ -1,4 +1,5 @@
-import styles from './style.module.scss'
+import { Link } from 'react-router-dom'; // 导入 Link 组件
+import styles from './style.module.scss';
 
 function Home() {
     return (
@@ -6,6 +7,11 @@ function Home() {
             <div style={{ textAlign: 'left' }}>
                 <h1 className={styles['title']}>Muliminty</h1>
                 <p className={styles['subtext']}>希望我们都能保持清醒和有意义的生活😁</p>
+                
+                {/* 增加关于按钮 */}
+                <Link to="/about" className={styles['aboutButton']}>
+                    关于
+                </Link>
             </div>
         </div>
     );
