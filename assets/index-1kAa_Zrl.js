c000268daa4a35ad5aced30f8ce4d29e5e37d6da
Muliminty <Muliminty@users.noreply.github.com>
2024-08-19 10:22:16 +0000

    deploy: 953523296044faea10009c83116889ef013c21dd

--- assets/index-1kAa_Zrl.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-main-CZElnpcG.js";const r=()=>t.jsx("div",{children:"About Page"});export{r as default};
---
+++ assets/index-QELwH2wn.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-main-CHvVHtTG.js";const r=()=>t.jsx("div",{children:"About Page"});export{r as default};
