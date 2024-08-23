903d5bff3fa2970dfb0eeb11b70a24fd2a05e3d5
muliminty <313195303@qq.com>
2024-08-23 16:37:36 +0800

    错误边界测试

--- src/home/page/about/index.jsx
+++ src/home/page/about/index.jsx
@@ -1,10 +1,16 @@
 // src/home/page/about/index.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Loading from '../../components/Loading/index';
 const About = () => {
-
-    return <div onClick={() => { throw new Error('Oops! Something went wrong.') }}>
+    const [flog, setFlog] = useState(false)
+    const ProblematicComponent = () => {
+        // 这个组件有可能会抛出错误
+        throw new Error('Oops! Something went wrong.')
+        return <div>This is a problematic component</div>
+    }
+    return <div onClick={() => { setFlog(!flog) }}>
         About Page
+        {flog && <ProblematicComponent />}
         {/* <Loading /> */}
     </div>;
 };
