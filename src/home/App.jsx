4c8272965254c34bb139683e962fd2652983a014
muliminty <313195303@qq.com>
2024-08-23 16:34:38 +0800

    错误边界

--- src/home/App.jsx
+++ src/home/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import AppRouter from './router';
-import './App.scss'
+import './App.scss';
 import ThemeSwitcher from '@/components/theme-switcher/index';
+import ErrorBoundary from '@/components/error-boundary/index';
+
 const App = () => {
   return (
     <div className="App">
-      <ThemeSwitcher />
-      <AppRouter />
+      <ErrorBoundary>
+        <ThemeSwitcher />
+        <AppRouter />
+      </ErrorBoundary>
     </div>
   );
 };
--- src/home/page/about/index.jsx
+++ src/home/page/about/index.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import Loading from '../../components/Loading/index';
 const About = () => {
-    return <div>
+
+    return <div onClick={() => { throw new Error('Oops! Something went wrong.') }}>
         About Page
         {/* <Loading /> */}
     </div>;
