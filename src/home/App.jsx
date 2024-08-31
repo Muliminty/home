bfb248a7a58ad7a8e542a97220eb6d04ba1c97e3
Muliminty <313195303@qq.com>
2024-08-31 12:10:19 +0800

    feat: 路由跳转

--- src/home/App.jsx
+++ src/home/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import AppRouter from './router';
 import './App.scss';
-import ThemeSwitcher from '@/components/theme-switcher/index';
 import ErrorBoundary from '@/components/error-boundary/index';
 
 const App = () => {
   return (
     <div className="App">
       <ErrorBoundary>
-        <ThemeSwitcher />
         <AppRouter />
       </ErrorBoundary>
     </div>
--- src/home/page/home/index.jsx
+++ src/home/page/home/index.jsx
@@ -1,16 +1,28 @@
 import { Link } from 'react-router-dom'; // 导入 Link 组件
 import styles from './style.module.scss';
 // import ThemeSwitcher from '@/components/theme-switcher/index'
+import ThemeSwitcher from '@/components/theme-switcher/index';
 
 function Home() {
     return (
         <div className={styles['container']}>
             <div style={{ textAlign: 'left' }}>
-                <h1 className={styles['title']}>Muliminty</h1>
+                <h1 className={styles['title']}>
+                    <ThemeSwitcher />
+                    Muliminty
+                </h1>
                 <p className={styles['subtext']}>希望我们都能保持清醒和有意义的生活😁</p>
 
-
+                <ul>
+                    <li>
+                        <Link to="/blog">+ blog test</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">+ about test</Link>
+                    </li>
+                </ul>
             </div>
+
         </div>
     );
 }
