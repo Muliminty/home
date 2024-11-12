670c4884e947e1092863842b05577bdf99a33646
muliminty <313195303@qq.com>
2024-11-12 09:48:16 +0800

    feat： note 自定义无序列表

--- src/home/components/ReactMarkdown/index.jsx
+++ src/home/components/ReactMarkdown/index.jsx
@@ -1,14 +1,19 @@
 import NotionImageRenderer from '@/home/components/ReactMarkdown/NotionImageRenderer';
-import LinkBtuton from '@/home/components/linkButton/index.jsx';
+import LinkButton from '@/home/components/linkButton/index.jsx';
 import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import CodeBlock from "./CodeBlock.jsx";
 import styles from './style.module.scss';
-import { useTheme } from '../../context/ThemeContext.jsx'; // 引入主题上下文
+import { useTheme } from '../../context/ThemeContext.jsx';
 import HoverButton from '@/home/components/hoverButton/index.jsx';
+import { ListItemRenderer } from './ListItemRenderer.jsx';
+
+// 递归计算嵌套级别
+const getLevel = (node) => node.parent && node.parent.tagName === 'ul' ? getLevel(node.parent) + 1 : 0;
+
 export const MarkdownRenderer = ({ data }) => {
-  const { theme } = useTheme(); // 使用上下文中的主题
+  const { theme } = useTheme();
   const [content, setContent] = useState('');
 
   useEffect(() => {
@@ -22,10 +27,12 @@ export const MarkdownRenderer = ({ data }) => {
         remarkPlugins={[remarkGfm]}
         components={{
           img: (props) => <NotionImageRenderer {...props} />,
-          code: (props) => <CodeBlock {...props} theme={theme} />, // 传递当前主题
-          a: (props) => {
-            return <HoverButton text={props.children} {...props} />
-          },
+          code: (props) => <CodeBlock {...props} theme={theme} />,
+          a: (props) => <HoverButton text={props.children} {...props} />,
+          ul: ({ children }) => <ul className={styles['custom-ul']}>{children}</ul>, // 自定义 ul 样式
+          li: ({ node, children }) => (
+            <ListItemRenderer level={getLevel(node)}>{children}</ListItemRenderer>
+          ), // 自定义 li 并传递层级
         }}
       />
     </div>
