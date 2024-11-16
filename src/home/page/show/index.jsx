2c78bb9f12abaae3c931b0b90edf9fa6a86e7a88
Muliminty <313195303@qq.com>
2024-11-16 18:18:56 +0800

    feat: 删除无用代码

--- src/home/page/show/index.jsx
+++ src/home/page/show/index.jsx
@@ -13,16 +13,27 @@ const Show = () => {
   const initialItems = [
     { id: 1, type: 'custom', content: <CardHover />, width: 2, height: 2 },
     { id: 2, type: 'about', content: <About />, width: 6, height: 2 },
-    { id: 3, type: 'themeSwitcher', content: <ThemeSwitcherCard />, width: 2, height: 2 },
-    { id: 4, type: 'image', content: <img className={styles.gridItemImageContent} src="https://ghchart.rshah.org/ee827c/Muliminty" alt="GitHub Chart" />, width: 6, height: 1 },
+    {
+      id: 3, type: 'themeSwitcher',
+      style: {
+        backgroundColor: theme === 'dark' ? '#26242d' : '#fff',
+      },
+      content: <ThemeSwitcherCard />, width: 2, height: 3
+    },
+
+    {
+      id: 4, type: 'image',
+      width: 4, height: 1,
+      content: <img className={styles.gridItemImageContent} src="https://ghchart.rshah.org/ee827c/Muliminty" alt="GitHub Chart" />,
+    },
     { id: 5, type: 'galleryManager', content: <GalleryManager />, width: 2, height: 3 },
-    // ...Array.from({ length: 7 }, (_, i) => ({
-    //   id: i + 6,
-    //   type: 'pending', // 类型为敬请期待
-    //   content: '敬请期待',
-    //   width: Math.ceil(Math.random() * 3),
-    //   height: Math.ceil(Math.random() * 3),
-    // })),
+    ...Array.from({ length: 7 }, (_, i) => ({
+      id: i + 6,
+      type: 'pending', // 类型为敬请期待
+      content: '敬请期待',
+      width: Math.ceil(Math.random() * 3),
+      height: Math.ceil(Math.random() * 3),
+    })),
   ];
 
   const [items, setItems] = useState(initialItems); // 当前显示的项目
@@ -61,7 +72,7 @@ const Show = () => {
     <div className={styles.showContainer}>
       <div className={styles.separator}>------------------------</div>
 
-      <button onClick={handleSort}>排序</button> {/* 添加排序按钮 */}
+      {/* <button onClick={handleSort}>排序</button> 添加排序按钮 */}
 
       <div className={styles.gridContainer}>
         {/* 渲染每个项 */}
@@ -76,7 +87,6 @@ const Show = () => {
               gridRow: `span ${height}`,
             }}
           >
-            {type === 'pending' && id}
             {/* 根据不同的类型来渲染内容 */}
             <div className={styles.cardContent}>
               {type === 'custom' && content}
