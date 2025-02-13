ed9d26342fe1a61246c3920753dcb1f9fcf3c2b3
Muliminty <313195303@qq.com>
2025-02-13 21:05:31 +0800

    feat: main 项目重构 init

--- src/home/components/ReactMarkdown/ListItemRenderer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './style.module.scss';
-
-// 自定义无序列表项组件，支持嵌套
-export const ListItemRenderer = ({ children, level = 0 }) => (
-  <li className={`${styles['custom-list-item']} ${styles[`list-level-${level}`]}`}>
-    <span className={styles['list-icon']}>&bull;</span> {/* 使用HTML的 • 符号作为项目符号 */}
-    <div className={styles['list-content']}>{children}</div>
-  </li>
-);
