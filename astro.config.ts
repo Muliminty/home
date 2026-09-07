d53d89c3c2fdbdfd118599625d792ead4791161e
muliminty <313195303@qq.com>
2026-09-07 10:40:34 +0800

    chore: 站点域名迁移到 muliminty.online
    - astro.config.ts 更新 site 字段
    - terminal.config.js 更新主页链接

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://muliminty.site',
+  site: 'https://muliminty.online',
   output: 'static',
   integrations: [react(), sitemap()],
   prefetch: {
